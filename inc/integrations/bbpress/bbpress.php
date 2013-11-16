a72666654597843f55a8d46ca701eb907a18601a
Spencer Finnell <spencerfinnell@gmail.com>
2013-11-16 11:12:02 -0500

    Home text for bbPress

--- inc/integrations/bbpress/bbpress.php
+++ inc/integrations/bbpress/bbpress.php
@@ -4,3 +4,21 @@
  *
  * @package Marketify
  */
+
+/**
+ * Override bbPress breadcrumb home text.
+ *
+ * Since our static homepage most likely has a long title,
+ * lets reset that to a standard home text.
+ *
+ * @since Marketify 1.0
+ *
+ * @param array $args
+ * @return array $args
+ */
+function marketify_bbp_before_get_breadcrumb_parse_args( $args ) {
+	$args[ 'home_text' ] = __( 'Home', 'marketify' );
+
+	return $args;
+}
+add_filter( 'bbp_before_get_breadcrumb_parse_args', 'marketify_bbp_before_get_breadcrumb_parse_args' );
