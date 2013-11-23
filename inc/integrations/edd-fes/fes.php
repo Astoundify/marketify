5e7bf8598d6c8ae2da3029bfde1f5596f19aeda9
Spencer Finnell <spencerfinnell@gmail.com>
2013-11-23 18:02:49 -0500

    Author image. Closes #101

---
+++ inc/integrations/edd-fes/fes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Easy Digital Downloads - Frontend Form Submission
+ *
+ * @package Marketify
+ */
+
+function marketify_edd_fes_vendor_archive_url() {
+
+}
+add_filter( 'edd_fes_vendor_archive_url', 'marketify_edd_fes_vendor_archive_url' );
