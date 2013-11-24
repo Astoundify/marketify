8e9fb717145b958e512b5fc0091ca8339a5b308f
Spencer Finnell <spencerfinnell@gmail.com>
2013-11-24 10:41:55 -0500

    Firefox fix messes other things up. See #110

--- inc/integrations/edd-fes/fes.php
+++ inc/integrations/edd-fes/fes.php
@@ -6,7 +6,6 @@
  */
 
 function marketify_edd_fes_vendor_archive_url( $url, $user ) {
-	wp_die( 'wat' );
 	return Marketify_Author::url( 'downloads', $user->ID );
 }
 add_filter( 'edd_fes_vendor_archive_url', 'marketify_edd_fes_vendor_archive_url', 10, 2 );
