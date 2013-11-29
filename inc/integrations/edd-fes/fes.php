e81412d647655efc984d01ec45aeb7572a45e12e
Spencer Finnell <spencerfinnell@gmail.com>
2013-11-29 18:21:28 -0500

    FES redirect removal. Closes #113

--- inc/integrations/edd-fes/fes.php
+++ inc/integrations/edd-fes/fes.php
@@ -5,6 +5,8 @@
  * @package Marketify
  */
 
+add_filter( 'edd_fes_vendor_archive_switch', '__return_false' );
+
 function marketify_edd_fes_vendor_archive_url( $url, $user ) {
 	return Marketify_Author::url( 'downloads', $user->ID );
 }
