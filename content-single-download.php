51651206494e286e9a4ab048899dfdc11d1e7577
Spencer Finnell <spencerfinnell@gmail.com>
2013-12-04 10:56:47 -0500

    Content hooks to output extra FES field if they want

--- content-single-download.php
+++ content-single-download.php
@@ -4,11 +4,17 @@
  */
 ?>
 
+<?php do_action( 'marketify_single_download_before' ); ?>
+
 <h2 class="section-title"><span><?php _e( 'About the Product', 'marketify' ); ?></span></h2>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="entry-content">
+		<?php do_action( 'marketify_single_download_content_before' ); ?>
+
 		<?php the_content(); ?>
+
+		<?php do_action( 'marketify_single_download_content_after' ); ?>
 
 		<?php
 			wp_link_pages( array(
@@ -20,3 +26,5 @@
 
 	<?php locate_template( array( 'modal-download-purchase.php' ), true, false ); ?>
 </article><!-- #post-## -->
+
+<?php do_action( 'marketify_single_download_after' ); ?>
