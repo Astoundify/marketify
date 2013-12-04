79b8bb78e1b19c3197e8297b51ee4ea1aea88920
Spencer Finnell <spencerfinnell@gmail.com>
2013-12-04 15:50:30 -0500

    Remove double recommdended manually. Closes #126

--- inc/integrations/edd-recommended/recommended.php
+++ inc/integrations/edd-recommended/recommended.php
@@ -60,7 +60,7 @@ add_action( 'marketify_single_download_after', 'marketify_recommended_products' 
  * @return void
  */
 function marketify_recommended_products_shim() {
-	remove_filter( 'edd_after_download_content', 'edd_rp_display_single' );
-	remove_filter( 'edd_after_checkout_cart', 'edd_rp_display_checkout' );
+	remove_filter( 'edd_after_download_content', 'edd_rp_display_single', 10, 1 );
+	remove_filter( 'edd_after_checkout_cart', 'edd_rp_display_checkout', 10, 1 );
 }
 add_action( 'init', 'marketify_recommended_products_shim' );
