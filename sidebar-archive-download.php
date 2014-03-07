568d875876db2e0d83b1fd2f7a54e6e53e31f2ed
Spencer Finell <spencerfinnell@gmail.com>
2014-03-07 15:56:08 -0500

    Only output the first file for audio types in the grid preview. Closes #223

--- sidebar-archive-download.php
+++ sidebar-archive-download.php
@@ -5,11 +5,11 @@
  * @package Marketify
  */
 
-if ( ! is_active_sidebar( 'sidebar-archive-download' ) )
+if ( ! is_active_sidebar( 'sidebar-download' ) )
 	return;
 ?>
 	<div id="secondary" class="download-archive-widget-area col-md-3 col-sm-5 col-xs-12" role="complementary">
 		<?php do_action( 'before_sidebar' ); ?>
 
-		<?php dynamic_sidebar( 'sidebar-archive-download' ); ?>
+		<?php dynamic_sidebar( 'sidebar-download' ); ?>
 	</div><!-- #secondary -->
