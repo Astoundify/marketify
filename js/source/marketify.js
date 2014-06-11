e42612a391e4a455960fb8eb4012bbac908ddfea
Spencer Finnell <spencerfinnell@gmail.com>
2014-06-11 14:00:28 -0500

    Checkout section title styling. Closes #315

--- js/source/marketify.js
+++ js/source/marketify.js
@@ -125,7 +125,7 @@ Marketify.App = ( function($) {
 				$( this ).addClass( 'active' );
 			});
 
-			$( '#bbpress-forums #bbp-user-wrapper h2.entry-title, #bbpress-forums fieldset.bbp-form legend, .fes-form h1, .fes-headers, .edd_form legend' ).wrapInner( '<span></span>' );
+			$( '#bbpress-forums #bbp-user-wrapper h2.entry-title, #bbpress-forums fieldset.bbp-form legend, .fes-form h1, .fes-headers' ).wrapInner( '<span></span>' );
 
 			$( '#edd_simple_shipping_fields legend' ).wrap( '<span></span>' );
 
