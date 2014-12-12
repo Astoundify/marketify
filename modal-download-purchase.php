fdc0b6beae6298b7162be307ad4b97551f0c5d56
Spencer Finnell <spencerfinnell@gmail.com>
2014-12-12 12:49:40 -0500

    Purchase form quantity styling.

--- modal-download-purchase.php
+++ modal-download-purchase.php
@@ -9,5 +9,5 @@ global $post;
 <div id="buy-now-<?php the_ID(); ?>" class="popup">
 	<h1 class="section-title"><span><?php _e( 'Buying Options', 'marketify' ); ?></span></h1>
 
-	<?php echo edd_get_purchase_link(); ?>
+	<?php echo edd_get_purchase_link( array( 'download_id' => $post->ID ) ); ?>
 </div>
