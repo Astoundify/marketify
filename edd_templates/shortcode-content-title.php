ee4a8521c2ab98b3409f49b236f8cdf8fa607d08
Spencer Finnell <spencerfinnell@gmail.com>
2015-01-19 14:59:42 -0500

    Make sure we always get the current author.

--- edd_templates/shortcode-content-title.php
+++ edd_templates/shortcode-content-title.php
@@ -23,10 +23,10 @@ global $post;
 				printf(
 					__( '<span class="byline"> by %1$s</span>', 'marketify' ),
 					sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s %4$s</a></span>',
-						marketify_edd_fes_author_url( get_the_author_meta( 'ID' ) ),
+						marketify_edd_fes_author_url( get_the_author_meta( 'ID', $post->post_author ) ),
 						esc_attr( sprintf( __( 'View all %s by %s', 'marketify' ), edd_get_label_plural(), get_the_author() ) ),
-						esc_html( get_the_author_meta( 'display_name' ) ),
-						get_avatar( get_the_author_meta( 'ID' ), 50, apply_filters( 'marketify_default_avatar', null ) )
+						esc_html( get_the_author_meta( 'display_name', $post->post_author ) ),
+						get_avatar( get_the_author_meta( 'ID', $post->post_author ), 50, apply_filters( 'marketify_default_avatar', null ) )
 					)
 				);
 			?>
