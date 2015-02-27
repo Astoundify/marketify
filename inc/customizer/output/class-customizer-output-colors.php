46f0280c9bbec57936959ab9d5adf7cf14c4005a
Spencer Finnell <spencerfinnell@gmail.com>
2015-02-27 12:26:17 -0500

    Add the rest of the customizer color outputs.

--- inc/customizer/output/class-customizer-output-colors.php
+++ inc/customizer/output/class-customizer-output-colors.php
@@ -8,6 +8,8 @@ class Marketify_Customizer_Output_Colors {
 		add_action( 'marketify_output_customizer_css', array( $this, 'page_header' ), 10 );
 		add_action( 'marketify_output_customizer_css', array( $this, 'primary' ), 30 );
 		add_action( 'marketify_output_customizer_css', array( $this, 'accent' ), 30 );
+		add_action( 'marketify_output_customizer_css', array( $this, 'footer' ), 40 );
+		add_action( 'marketify_output_customizer_css', array( $this, 'overlay' ), 40 );
 	}
 
 	public function page_header() {
@@ -63,11 +65,168 @@ class Marketify_Customizer_Output_Colors {
 			)
 		) );
 
+		$this->css->add( array(
+			'selectors' => array(
+				'button',
+				'input[type=reset]',
+				'input[type=submit]',
+				'.button',
+				'a.button',
+				'.fes-button',
+				'.edd_price_options input[type=radio]:checked',
+				'body #edd-wl-modal input[type=radio]:checked',
+				'#edd_checkout_form_wrap fieldset#edd_cc_fields legend',
+				'.marketify_widget_featured_popular .home-widget-title span:hover',
+				'.marketify_widget_featured_popular .home-widget-title span.active',
+				'.entry-content blockquote',
+				'.nav-previous a:hover',
+				'.nav-next a:hover',
+				'body a.edd-wl-action',
+				'body a.edd-wl-action.edd-wl-button',
+				'body a.edd-wl-action.edd-wl-button:hover',
+				'.fes-feat-image-btn',
+				'.upload_file_button',
+				'.fes-avatar-image-bt'
+			),
+			'declarations' => array(
+				'border-color' => $primary
+			)
+		) );
+
+		$this->css->add( array(
+			'selectors' => array(
+				'button:hover',
+				'input[type=reset]:hover',
+				'input[type=submit]:hover',
+				'.button:hover',
+				'a.button:hover',
+				'#edd_checkout_form_wrap fieldset#edd_cc_fields > span:after',
+				'.edd-reviews-voting-buttons a:hover',
+				'.flex-control-nav a.flex-active',
+				'.search-form .search-submit',
+				'.fes-pagination a.page-numbers:hover',
+				'body a.edd-wl-action.edd-wl-button:hover',
+				'.fes-feat-image-btn:hover',
+				'.upload_file_button:hover'
+			),
+			'declarations' => array(
+				'background-color' => $primary
+			)
+		) );
+
 	}
 
 	public function accent() {
 		$accent = marketify_theme_mod( 'color-accent' );
 	
+		$this->css->add( array(
+			'selectors' => array(
+				'a.edd-cart-saving-button',
+				'input[name=edd_update_cart_submit]',
+				'.main-navigation .edd-cart .cart_item.edd_checkout a',
+				'.download-variable .entry-content .edd-add-to-cart.button.edd-submit:hover',
+				'.download-variable .entry-content .edd_go_to_checkout.button.edd-submit:hover',
+				'.popup .edd-add-to-cart.button.edd-submit:hover',
+				'.edd-reviews-voting-buttons a',
+				'a.edd-fes-adf-submission-add-option-button',
+				'#fes-insert-image',
+				'#fes-view-comment a',
+				'a.edd_terms_link',
+			),
+			'declarations' => array(
+				'color' => $accent
+			)
+		) );
+
+		$this->css->add( array(
+			'selectors' => array(
+				'a.edd-cart-saving-button',
+				'input[name=edd_update_cart_submit]',
+				'.main-navigation .edd-cart .cart_item.edd_checkout a:hover',
+				'.download-variable .entry-content .edd-add-to-cart.button.edd-submit:hover',
+				'.download-variable .entry-content .edd_go_to_checkout.button.edd-submit:hover',
+				'.popup .edd-add-to-cart.button.edd-submit:hover',
+				'.popup .edd_go_to_checkout.button.edd-submit',
+				'.popup .edd_go_to_checkout.button.edd-submit:hover',
+				'.edd-reviews-voting-buttons a',
+				'.edd-fes-adf-submission-add-option-button',
+				'#fes-insert-image',
+				'#fes-view-comment a',
+				'.edd_terms_links',
+				'.site-footer.dark .mailbag-wrap input[type=submit]',
+				'.insert-file-row',
+			),
+			'declarations' => array(
+				'border-color' => $accent
+			)
+		) );
+
+		$this->css->add( array(
+			'selectors' => array(
+				'a.edd-cart-saving-button:hover',
+				'input[name=edd_update_cart_submit]:hover',
+				'.minimal #edd_purchase_submit input[type=submit]',
+				'.main-navigation .edd-cart .cart_item.edd_checkout a:hover',
+				'.minimal a.edd-cart-saving-button',
+				'.minimal input[name=edd_update_cart_submit]',
+				'.minimal .fes-form input[type=submit]',
+				'.popup .edd_go_to_checkout.button.edd-submit',
+				'.popup .edd_go_to_checkout.button.edd-submit:hover',
+				'.main-navigation .search-form.active .search-submit',
+				'.main-navigation.toggled .search-form .search-submit',
+				'.edd-fes-adf-submission-add-option-button:hover',
+				'#fes-insert-image:hover',
+				'.edd-reviews-voting-buttons a:hover',
+				'.minimal #edd_login_submit',
+				'.minimal input[name=edd_register_submit]',
+				'.edd_terms_links:hover',
+				'.site-footer.dark .mailbag-wrap input[type=submit]',
+				'.home-search .page-header .search-submit',
+				'.search-form-overlay .search-submit',
+				'.marketify_widget_taxonomy_stylized',
+				'.insert-file-row',
+			),
+			'declarations' => array(
+				'border-color' => $accent
+			)
+		) );
+	}
+
+	public function footer() {
+		$this->css->add( array(
+			'selectors' => array(
+				'.site-footer.light'
+			),
+			'declarations' => array(
+				'color' => '#' . get_theme_mod( 'background_color' )
+			)
+		) );
+	}
+
+	public function overlay() {
+		$primary = marketify_theme_mod( 'color-primary' );
+
+		$this->css->add( array(
+			'selectors' => array(
+				'.content-grid-download .entry-image:hover .overlay',
+				'.content-grid-download .entry-image.hover .overlay',
+				'.download-image-grid-preview .slides li.active a:before',
+				'.download-image-grid-preview .slides li:hover a:before',
+			),
+			'declarations' => array(
+				'background' => 'rgba(' . $this->css->hex2rgb( $primary ) . ',.80)',
+				'border' => '1px solid rgba(' . $this->css->hex2rgb( $primary ) . ',.80)',
+			)
+		) );
+
+		$this->css->add( array(
+			'selectors' => array(
+				'.search-form-overlay',
+			),
+			'declarations' => array(
+				'background-color' => 'rgba(' . $this->css->hex2rgb( $primary ) . ', .90)',
+			)
+		) );
 	}
 
 }
