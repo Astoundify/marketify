19668ed87e84647d7b43c10b81d0c7f482b615b1
Spencer Finnell <spencerfinnell@gmail.com>
2015-07-09 10:54:20 -0400

    Start some header and menu sanity.

--- header.php
+++ header.php
@@ -25,27 +25,35 @@
 
 		<header id="masthead" class="site-header" role="banner">
 			<div class="container">
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<h1 class="menu-toggle"><i class="icon-list2"></i></h1>
 
-					<?php if ( ! ( is_front_page() && is_page_template( 'page-templates/home-search.php' ) ) ) : ?>
-					<div class="header-search-icon"><i class="icon-search"></i></div>
-					<?php endif; ?>
+				<div class="site-header-inner">
 
-					<?php locate_template( array( 'searchform.php' ), true ); ?>
+					<div class="site-branding">
+						<?php $header_image = get_header_image(); ?>
+						<?php if ( ! empty( $header_image ) ) : ?>
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="custom-header"><img src="<?php echo esc_url( $header_image ); ?>" alt=""></a>
+						<?php endif; ?>
 
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'main-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<h2 class="site-description screen-reader-text"><?php bloginfo( 'description' ); ?></h2>
+					</div>
 
-				<div class="site-branding">
-					<?php $header_image = get_header_image(); ?>
-					<?php if ( ! empty( $header_image ) ) : ?>
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="custom-header"><img src="<?php echo esc_url( $header_image ); ?>" alt=""></a>
-					<?php endif; ?>
+					<div class="nav-menu nav-menu--primary">
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'primary',
+								'container_class' => 'nav-menu-container'
+							) );
+						?>
 
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<h2 class="site-description screen-reader-text"><?php bloginfo( 'description' ); ?></h2>
+						<a href="#" class="navigation-bar-toggle">
+							<i class="ion-navicon-round"></i>
+							Menu
+						</a>
+					</div>
+
 				</div>
+
 			</div>
 		</header><!-- #masthead -->
 
