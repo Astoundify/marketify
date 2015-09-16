25afebe731fab426f2571ff4fcc1d8d11effc776
Spencer Finnell <spencerfinnell@gmail.com>
2015-09-16 16:26:35 -0400

    tweaks

--- inc/integrations/bbpress/class-bbpress.php
+++ inc/integrations/bbpress/class-bbpress.php
@@ -1,26 +1,19 @@
 <?php
 
 class Marketify_bbPress extends Marketify_Integration {
-	
-	public function __construct() {
-		parent::__construct( dirname( __FILE__ ) );
-	}
 
-	public function setup_actions() {
-		add_filter( 'bbp_default_styles', array( $this, 'default_styles' ) );
-		add_filter( 'bbp_before_get_breadcrumb_parse_args', 'breadcrumb_args' );
-	}
+    public function __construct() {
+        parent::__construct( dirname( __FILE__ ) );
+    }
 
-	public function default_styles( $styles ) {
-		$styles[ 'bbp-default' ][ 'file' ] = '';
+    public function setup_actions() {
+        add_filter( 'bbp_before_get_breadcrumb_parse_args', array( $this, 'breadcrumb_args' ) );
+    }
 
-		return $styles;
-	}
+    public function breadcrumb_args( $args ) {
+        $args[ 'home_text' ] = __( 'Home', 'marketify' );
 
-	public function breadcrumb_args( $args ) {
-		$args[ 'home_text' ] = __( 'Home', 'marketify' );
-
-		return $args;
-	}
+        return $args;
+    }
 
 }
