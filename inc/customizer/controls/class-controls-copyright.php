68cfaf9a85ff21c9f705cc346b9347865522fb04
Spencer Finnell <spencerfinnell@gmail.com>
2015-10-02 08:42:49 -0400

    Rows. Closes #471

--- inc/customizer/controls/class-controls-copyright.php
+++ inc/customizer/controls/class-controls-copyright.php
@@ -2,37 +2,37 @@
 
 class Marketify_Customizer_Controls_Footer_Copyright extends Marketify_Customizer_Controls {
 
-	public $controls = array();
+    public $controls = array();
 
-	public function __construct() {
-		$this->section = 'copyright';
-		$this->priority = new Marketify_Customizer_Priority(49, 1);
+    public function __construct() {
+        $this->section = 'copyright';
+        $this->priority = new Marketify_Customizer_Priority(49, 1);
 
-		parent::__construct();
+        parent::__construct();
 
-		add_action( 'customize_register', array( $this, 'add_controls' ), 30 );
-		add_action( 'customize_register', array( $this, 'set_controls' ), 35 );
-	}
+        add_action( 'customize_register', array( $this, 'add_controls' ), 30 );
+        add_action( 'customize_register', array( $this, 'set_controls' ), 35 );
+    }
 
-	public function add_controls( $wp_customize ) {
-		$this->controls = array(
-			'footer-copyright-display' => array(
-				'label' => __( 'Display "Copyright" Section', 'marketify' ),
-				'type'    => 'checkbox'
-			),
-			'footer-copyright-logo' => array(
-				'label' => __( 'Logo', 'marketify' ),
-				'type'    => 'WP_Customize_Image_Control'
-			),
-			'footer-copyright-text' => array(
-				'label' => __( 'Copyright', 'marketify' ),
-				'type'    => 'Marketify_Customize_Textarea_Control'
-			),
-		);
+    public function add_controls( $wp_customize ) {
+        $this->controls = array(
+            'footer-copyright-display' => array(
+                'label' => __( 'Display "Copyright" Section', 'marketify' ),
+                'type'    => 'checkbox'
+            ),
+            'footer-copyright-logo' => array(
+                'label' => __( 'Logo', 'marketify' ),
+                'type'    => 'WP_Customize_Image_Control'
+            ),
+            'footer-copyright-text' => array(
+                'label' => __( 'Copyright', 'marketify' ),
+                'type' => 'Marketify_Customize_Textarea_Control',
+            ),
+        );
 
-		return $this->controls;
-	}
-	
+        return $this->controls;
+    }
+
 }
 
 new Marketify_Customizer_Controls_Footer_Copyright();
