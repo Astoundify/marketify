e608d57fb78034b8f5d6857afa75521a1096b2f7
Spencer Finnell <spencerfinnell@gmail.com>
2015-10-07 09:18:11 -0400

    blog pagination. see #475

--- inc/template/class-template-pagination.php
+++ inc/template/class-template-pagination.php
@@ -2,28 +2,28 @@
 
 class Marketify_Template_Pagination {
 
-	public function __construct() {
-		add_action( 'marketify_loop_after', array( $this, 'output' ) );
-	}
+    public function __construct() {
+        add_action( 'marketify_loop_after', array( $this, 'output' ) );
+    }
 
-	public function output() {
-		global $wp_query;
+    public function output() {
+        global $wp_query;
 
-		$big = 999999999;
+        $big = 999999999;
 
-		$links = paginate_links( array(
-			'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-			'format' => '?paged=%#%',
-			'current' => max( 1, get_query_var('paged') ),
-			'total' => $wp_query->max_num_pages,
-			'prev_text' => '<i class="icon-left-open-big"></i>',
-			'next_text' => '<i class="icon-right-open-big"></i>'
-		) );
-	?>
-		<div class="paginate-links container">
-			<?php echo $links; ?>
-		</div>
-	<?php
-	}
+        $links = paginate_links( array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => max( 1, get_query_var('paged') ),
+            'total' => $wp_query->max_num_pages,
+            'prev_text' => __( 'Previous', 'marketify' ),
+            'next_text' => __( 'Next', 'marketify' )
+        ) );
+    ?>
+        <div class="paginate-links container">
+            <?php echo $links; ?>
+        </div>
+    <?php
+    }
 
 }
