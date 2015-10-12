7196b1231e8c93500631026cace8c3d221845655
Spencer Finnell <spencerfinnell@gmail.com>
2015-10-12 14:24:50 -0400

    some modular css

--- inc/integrations/edd-recommended-products/class-edd-recommended-products.php
+++ inc/integrations/edd-recommended-products/class-edd-recommended-products.php
@@ -2,60 +2,62 @@
 
 class Marketify_EDD_Recommended_Products extends Marketify_Integration {
 
-	public function __construct() {
-		parent::__construct( dirname( __FILE__ ) );
-	}
+    public function __construct() {
+        parent::__construct( dirname( __FILE__ ) );
+    }
 
-	public function setup_actions() {
-		add_action( 'init', array( $this, 'remove_auto_output' ), 12 );
-		add_action( 'marketify_single_download_after', array( $this, 'output' ) );
-	}	
+    public function setup_actions() {
+        add_action( 'init', array( $this, 'remove_auto_output' ), 12 );
+        add_action( 'marketify_single_download_after', array( $this, 'output' ) );
+    }	
 
-	public function remove_auto_output() {
-		remove_filter( 'edd_after_download_content', 'edd_rp_display_single', 10, 1 );
-		remove_filter( 'edd_after_checkout_cart', 'edd_rp_display_checkout' );
-	}
+    public function remove_auto_output() {
+        remove_filter( 'edd_after_download_content', 'edd_rp_display_single', 10, 1 );
+        remove_filter( 'edd_after_checkout_cart', 'edd_rp_display_checkout' );
+    }
 
-	public function output() {
-		if ( is_singular( 'download' ) ) {
-			global $post;
+    public function output() {
+        if ( is_singular( 'download' ) ) {
+            global $post;
 
-			$suggestion_data = edd_rp_get_suggestions( $post->ID );
-		} else {
-			$cart_items = edd_get_cart_contents();
+            $suggestion_data = edd_rp_get_suggestions( $post->ID );
+        } else {
+            $cart_items = edd_get_cart_contents();
 
-			if ( empty( $cart_items ) ) {
-				return;
-			}
+            if ( empty( $cart_items ) ) {
+                return;
+            }
 
-			$post_ids        = wp_list_pluck( $cart_items, 'id' );
-			$user_id         = is_user_logged_in() ? get_current_user_id() : false;
-			$suggestion_data = edd_rp_get_multi_suggestions( $post_ids, $user_id );
-		}
+            $post_ids        = wp_list_pluck( $cart_items, 'id' );
+            $user_id         = is_user_logged_in() ? get_current_user_id() : false;
+            $suggestion_data = edd_rp_get_multi_suggestions( $post_ids, $user_id );
+        }
 
-		if ( ! is_array( $suggestion_data ) || empty( $suggestion_data ) ) {
-			return;
-		}
+        if ( ! is_array( $suggestion_data ) || empty( $suggestion_data ) ) {
+            return;
+        }
 
-		$suggestions = array_keys( $suggestion_data );
+        $suggestions = array_keys( $suggestion_data );
 
-		$suggested_downloads = new WP_Query( array(
-			'post__in'       => $suggestions,
-			'post_type'      => 'download',
-			'posts_per_page' => edd_get_option( 'edd_rp_suggestion_count' )
-		) );
-	?>
+        $suggested_downloads = new WP_Query( array(
+            'post__in'       => $suggestions,
+            'post_type'      => 'download',
+            'posts_per_page' => edd_get_option( 'edd_rp_suggestion_count' )
+        ) );
+    ?>
 
-		<h1 class="section-title recommended-products"><span><?php _e( 'Recommended Products', 'marketify' ); ?></span></h1>
+        <div class="edd-recommended-products">
+            <h3 class="section-title recommended-products"><span><?php _e( 'Recommended Products', 'marketify' ); ?></span></h3>
 
-		<div class="row edd-recommended-products">
-			<?php while ( $suggested_downloads->have_posts() ) : $suggested_downloads->the_post(); ?>
-			<div class="col-lg-3 col-md-4 col-sm-6">
-				<?php get_template_part( 'content-grid', 'download' ); ?>
-			</div>
-			<?php endwhile; ?>
-		</div>
-	<?php
-	}
+            <div class="row">
+                <?php while ( $suggested_downloads->have_posts() ) : $suggested_downloads->the_post(); ?>
+                <div class="col-lg-3 col-md-4 col-sm-6">
+                    <?php get_template_part( 'content-grid', 'download' ); ?>
+                </div>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    <?php
+    }
 
 }
