82525a6e86928f88372ef223a0053fdcb6dcf044
Spencer Finnell <spencerfinnell@gmail.com>
2015-11-05 15:14:42 -0500

    Download Meta widget.

--- inc/integrations/edd-fes/widgets/class-widget-product-details.php
+++ inc/integrations/edd-fes/widgets/class-widget-product-details.php
@@ -12,173 +12,30 @@ class Marketify_Widget_FES_Product_Details extends Marketify_Widget {
                 'type'  => 'text',
                 'std'   => '',
                 'label' => __( 'Title:', 'marketify' )
-            ),
-            'fields' => array(
-                'type'  => 'multicheck',
-                'label' => __( 'Fields to Output:', 'marketify' ),
-                'std' => '',
-                'options' => $this->get_fields_list()
-            ),
+            )
         );
         parent::__construct();
     }
 
     function widget( $args, $instance ) {
+        global $post;
+
         $title  = apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base );
-        $chosen = isset( $instance[ 'fields' ] ) ? $instance[ 'fields' ] : array();
-        $chosen = maybe_unserialize( $chosen );
-
-        $output = $this->get_product_details_output( $chosen );
-
-        if ( empty( $output ) ) {
-            return;
-        }
 
         echo $args[ 'before_widget' ];
 
         if ( $title ) {
             echo $args[ 'before_title' ] . $title . $args[ 'after_title' ];
         }
-    ?>
 
-        <table class="edd-fpd">
-            <?php foreach ( $output as $label => $value ) : if ( '' == $value ) continue; ?>
-            <tr>
-                <th><?php echo $label; ?></th>
-                <td><?php echo $value; ?></td>
-            </tr>
-            <?php endforeach; ?>
-        </table>
+        $form_id = EDD_FES()->helper->get_option( 'fes-submission-form', false );
 
-        <?php
+        if ( $form_id ) {
+            $form = EDD_FES()->helper->get_form_by_id( $form_id, $post->ID );
+            echo $form->display_fields();
+        }
+
         echo $args[ 'after_widget' ];
-    }
-
-    private function get_fields_list() {
-        $fields  = $this->get_form_fields();
-
-        if ( ! is_array( $fields ) ) {
-            return array();
-        }
-
-        $labels = wp_list_pluck( $fields, 'label' );
-        $names  = wp_list_pluck( $fields, 'name' );
-
-        $options = array_combine( $names, $labels );
-        $options = array_filter( $options );
-
-        return $options;
-    }
-
-    public function get_form_fields() {
-        $form_id = EDD_FES()->helper->get_option( 'fes-submission-form' );
-
-        if ( ! $form_id ) {
-            return array();
-        }
-
-        $fields  = get_post_meta( $form_id, 'fes-form', true );
-
-        return $fields;
-    }
-
-    public function get_product_details_output( $chosen ) {
-        global $post;
-
-        $fields  = $this->get_form_fields();
-        $meta    = array();
-
-        if ( ! $fields ) {
-            return;
-        }
-
-        foreach ( $fields as $field ) {
-            if ( ! in_array( $field[ 'name' ], $chosen ) ) {
-                continue;
-            }
-
-            $value = get_post_meta( $post->ID, $field[ 'name' ], true );
-
-            switch ( $field[ 'input_type' ] ) {
-                case 'image_upload' :
-                    if ( 'featured_image' == $field[ 'template' ] ) {
-                        $value = get_the_post_thumbnail( $post->ID, 'thumbnail' );
-                    }
-                break;
-
-                case 'file_upload' :
-                    $uploads = array();
-
-                    $value = '';
-
-                    if ( is_array( $value ) ) {
-                        foreach ( $value as $attachment_id ) {
-                            $uploads[] = wp_get_attachment_link( $attachment_id, 'thumbnail', false, true );
-                        }
-
-                        $value = implode( '<br />', $uploads );
-                    }
-                break;
-
-                case 'checkbox' :
-                case 'multiselect' :
-                    if ( ! is_array( $value ) ) {
-                        $value = explode( '|', $value );
-                    } else {
-                        $value = array_map( 'trim', $value );
-                    }
-
-                    $value = implode( $this->multi_sep, $value );
-                break;
-
-                case 'taxonomy' :
-                    $terms = wp_get_post_terms( $post->ID, $field[ 'name' ] );
-
-                    if ( ! is_wp_error( $terms ) && ! empty( $terms ) ) {
-                        switch ( $field[ 'type' ] ) {
-
-                            case 'checkbox' :
-                            case 'multiselect' :
-                            case 'text' :
-                                $_terms = array();
-
-                                foreach ( $terms as $term ) {
-                                    $_terms[] = sprintf( '<a href="%s">%s</a>', get_term_link( $term, $field[ 'name' ] ), $term->name );
-                                }
-
-                                $value = implode( $this->multi_sep, $_terms );
-                            break;
-
-                            case 'select' :
-                                $value = sprintf( '<a href="%s">%s</a>', get_term_link( current( $terms ), $field[ 'name' ] ), current( $terms )->name );
-                            break;
-
-                        }
-                    }
-                break;
-
-                default :
-                    if ( 'no' != $field[ 'is_meta' ] ) {
-                        $value = get_post_meta( $post->ID, $field[ 'name' ], true );
-                    } else {
-                        $value = get_post_field( $field[ 'name' ], $post->ID );
-                    }
-                break;
-            }
-
-            $value = make_clickable( $value );
-
-            $label = apply_filters( 'edd_fpd_label', isset( $field[ 'label' ] ) ? $field[ 'label' ] : '', $field );
-            $value = apply_filters( 'edd_fpd_value', $value, $field );
-
-            if ( empty( $value ) )
-                continue;
-
-            $meta[ $label ] = $value;
-
-        }
-
-        return $meta;
     }
 
 }
