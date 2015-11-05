c3ff185ee83c3ddc2ce4dc80dfac0794891990e6
Spencer Finnell <spencerfinnell@gmail.com>
2015-11-05 14:20:48 -0500

    align images to the left. see #407

--- inc/setup/steps/import-widgets.php
+++ inc/setup/steps/import-widgets.php
@@ -1,9 +1,9 @@
+<p class="alignleft"><a href="<?php echo get_template_directory_uri(); ?>/inc/setup/images/setup-widgets.gif">
+	<img src="<?php echo get_template_directory_uri(); ?>/inc/setup/images/setup-widgets.gif" width="430" alt="" />
+</a>
+
 <p>Importing the demo widgets is not required to use this theme. It simply allows you to quickly match the same settings
 found on our theme demo. If you do not import the widgets you can manually manage your widgets just like a standard
 WordPress widget area.</p>
-
-<p><a href="<?php echo get_template_directory_uri(); ?>/inc/setup/images/setup-widgets.gif">
-	<img src="<?php echo get_template_directory_uri(); ?>/inc/setup/images/setup-widgets.gif" width="430" alt="" />
-</a>
 
 <p><a href="<?php echo admin_url( 'tools.php?page=widget-importer-exporter' ); ?>" class="button button-primary button-large">Begin Importing Widgets</a></p>
--- inc/setup/steps/install-plugins.php
+++ inc/setup/steps/install-plugins.php
@@ -1,3 +1,7 @@
+<p class="alignleft"><a href="<?php echo get_template_directory_uri(); ?>/inc/setup/images/setup-plugins.gif">
+    <img src="<?php echo get_template_directory_uri(); ?>/inc/setup/images/setup-plugins.gif" width="430" alt="" />
+</a></p>
+
 <p>Before you can use Marketify you must first install Easy Digital Downloads at minimum. You can read about <a href="http://jobify.astoundify.com/article/260-why-does-this-theme-require-plugins">why this theme requires plugins</a> in our documentation.</p> 
 
 <p><strong>Note:</strong></p>
@@ -6,9 +10,5 @@
     <li>It is recommended you install and activate any additional plugins you plan on using before importing any XML content.</li>
     <li>Once your plugins are installed and content is imported please review all plugin settings pages to make sure everything has been properly set up.</li>
 </ul>
-
-<p><a href="<?php echo get_template_directory_uri(); ?>/inc/setup/images/setup-plugins.gif">
-    <img src="<?php echo get_template_directory_uri(); ?>/inc/setup/images/setup-plugins.gif" width="430" alt="" />
-</a></p>
 
 <p><a href="<?php echo admin_url( 'themes.php?page=tgmpa-install-plugins' ); ?>" class="button button-primary button-large">Install Plugins</a></p>
