0bcc050fe53ad273adfcb991de9ff7dfb322298e
Spencer Finnell <spencerfinnell@gmail.com>
2015-11-05 11:29:16 -0500

    fes setup wording

--- inc/setup/steps/setup-fes.php
+++ inc/setup/steps/setup-fes.php
@@ -1,8 +1,8 @@
 <p>When Easy Digital Downloads - Frontend Submissions is activated it will automatically create pages for your store. You need to do a few things to complete this setup:</p>
 
 <ol>
-<li><a href="<?php echo admin_url( 'nav-menus.php' ); ?>">Add the Frontend Submissino pages to your Menu</a></li>
-<li><a href="<?php echo admin_url( 'admin.php?page=fes-settings' ); ?>">Configure your Frontend Submission settings</a></li>
+<li><a href="<?php echo admin_url( 'nav-menus.php' ); ?>">Add the Frontend Submissions pages to your Menu</a></li>
+<li><a href="<?php echo admin_url( 'admin.php?page=fes-settings' ); ?>">Turn off the Frontend Submission's CSS</a></li>
 <li><a href="<?php echo admin_url( 'post.php?post=' . EDD_FES()->helper->get_option( 'fes-submission-form', false ) ); ?>">Configure your Submission form</a></li>
 <li><a href="<?php echo admin_url( 'post.php?post=' . EDD_FES()->helper->get_option( 'fes-registration-form', false ) ); ?>">Configure your Registration form</a></li>
 <li><a href="<?php echo admin_url( 'post.php?post=' . EDD_FES()->helper->get_option( 'fes-profile-form', false ) ); ?>">Configure your Profile form</a></li>
