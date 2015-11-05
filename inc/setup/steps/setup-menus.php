968089a08ce852a6d38c0eb433949e7208d5b2f2
Spencer Finnell <spencerfinnell@gmail.com>
2015-11-05 13:52:37 -0500

    link to menu locations.

--- inc/setup/steps/setup-menus.php
+++ inc/setup/steps/setup-menus.php
@@ -6,4 +6,4 @@
 	<img src="<?php echo get_template_directory_uri(); ?>/inc/setup/images/setup-menus.gif" width="430" alt="" />
 </a></p>
 
-<p><a href="<?php echo admin_url( 'nav-menus.php' ); ?>" class="button button-primary button-large">Manage Menus</a></p>
+<p><a href="<?php echo admin_url( 'nav-menus.php?action=locations' ); ?>" class="button button-primary button-large">Manage Menus</a></p>
