c9c93c167f084e6e9b0b6613a1eb1a8128beb743
Spencer Finnell <spencerfinnell@gmail.com>
2015-11-09 10:21:27 -0500

    remove custom user contact methods. closes #533

--- inc/class-helpers.php
+++ inc/class-helpers.php
@@ -2,10 +2,6 @@
 
 class Marketify_Helpers {
 
-    public function __construct() {
-        add_filter( 'user_contactmethods', array( $this, 'user_contactmethods' ), 10, 2 );
-    }
-
     public function apply_date_query( $query_args, $timeframe ) {
         if ( 'all' == $timeframe ) {
             return $query_args;
@@ -26,15 +22,4 @@ class Marketify_Helpers {
         return $query_args;
     }
 
-    public function user_contactmethods( $methods, $user ) {
-        unset( $methods[ 'aim' ] );
-        unset( $methods[ 'yim' ] );
-        unset( $methods[ 'jabber' ] );
-
-        $methods[ 'twitter' ]  = 'Twitter URL';
-        $methods[ 'facebook' ] = 'Facebook URL';
-        $methods[ 'gplus' ]    = 'Google+ URL';
-
-        return $methods;
-    }
 }
