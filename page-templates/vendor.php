1ae212f310ac6ad0211e5e17cffbceb71f101a3a
Spencer Finnell <spencerfinnell@gmail.com>
2015-11-10 14:13:50 -0500

    move vendor sidebear to the right. closes #279

--- page-templates/vendor.php
+++ page-templates/vendor.php
@@ -9,10 +9,22 @@
 
 get_header(); ?>
 
-	<?php do_action( 'marketify_entry_before' ); ?>
+    <?php do_action( 'marketify_entry_before' ); ?>
 
     <div class="container">
         <div id="content" class="site-content row">
+
+            <section id="primary" class="content-area col-md-9 col-sm-7 col-xs-12">
+                <main id="main" class="site-main" role="main">
+
+                    <?php while ( have_posts() ) : the_post(); ?>
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                    </div>
+                    <?php endwhile; ?>
+
+                </main><!-- #main -->
+            </section><!-- #primary -->
 
             <div id="secondary" class="author-widget-area col-md-3 col-sm-5 col-xs-12" role="complementary">
                 <div class="vendor-widget-area">
@@ -32,18 +44,6 @@ get_header(); ?>
                     ?>
                 </div>
             </div><!-- #secondary -->
-
-            <section id="primary" class="content-area col-md-9 col-sm-7 col-xs-12">
-                <main id="main" class="site-main" role="main">
-
-                    <?php while ( have_posts() ) : the_post(); ?>
-                    <div class="entry-content">
-                        <?php the_content(); ?>
-                    </div>
-                    <?php endwhile; ?>
-
-                </main><!-- #main -->
-            </section><!-- #primary -->
 
         </div><!-- #content -->
     </div>
