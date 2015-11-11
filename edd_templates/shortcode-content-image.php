4103452de243735fb94088dbc129929cb3a9a61a
Spencer Finnell <spencerfinnell@gmail.com>
2015-11-11 11:51:21 -0500

    Add some more deprecated functions

--- edd_templates/shortcode-content-image.php
+++ edd_templates/shortcode-content-image.php
@@ -9,21 +9,21 @@ global $post;
 <?php do_action( 'marketify_download_content_image_before' ); ?>
 
 <div class="content-grid-download__entry-image">
-	<div class="content-grid-download__overlay">
-		<?php do_action( 'marketify_download_content_image_overlay_before' ); ?>
+    <div class="content-grid-download__overlay">
+        <?php do_action( 'marketify_download_content_image_overlay_before' ); ?>
 
-		<div class="content-grid-download__actions">
-			<?php do_action( 'marketify_download_content_actions_before' ); ?>
+        <div class="content-grid-download__actions">
+            <?php do_action( 'marketify_download_content_actions_before' ); ?>
 
-			<a href="<?php the_permalink(); ?>" rel="bookmark" class="button button--color-white"><?php _e( 'Details', 'marketify' ); ?></a>
+            <a href="<?php the_permalink(); ?>" rel="bookmark" class="button button--color-white"><?php _e( 'Details', 'marketify' ); ?></a>
 
-			<strong class="item-price"><span><?php printf( __( 'Item Price: %s', 'marketify' ), edd_price( get_the_ID(), false ) ); ?></span></strong>
+            <strong class="item-price"><span><?php printf( __( 'Item Price: %s', 'marketify' ), edd_price( get_the_ID(), false ) ); ?></span></strong>
 
-			<?php do_action( 'marketify_download_content_actions_after' ); ?>
-		</div>
-	</div>
+            <?php do_action( 'marketify_download_content_actions_after' ); ?>
+        </div>
+    </div>
 
-	<?php the_post_thumbnail( 'medium' ); ?>
+    <?php the_post_thumbnail( 'medium' ); ?>
 </div>
 
 <?php locate_template( array( 'modal-download-purchase.php' ), true, false ); ?>
--- inc/deprecated.php
+++ inc/deprecated.php
@@ -6,3 +6,13 @@
 function marketify_purchase_link( $download_id ) {
     marketify()->get( 'edd' )->purchase_form->purchase_link( $download_id );
 }
+
+function marketify_is_multi_vendor() {
+    return true;
+}
+
+function marketify_edd_fes_author_url( $id ) {
+    $vendor = Marketify_EDD_FES_Vendor( $id );
+
+    return $vendor->url();
+}
