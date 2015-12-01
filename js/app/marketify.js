4a16dad10d49b3ebb1092f744b0a134dc19601f0
Spencer Finnell <spencerfinnell@gmail.com>
2015-12-01 09:07:57 -0500

    Use a popup gallery on standard downloads

--- js/app/marketify.js
+++ js/app/marketify.js
@@ -130,6 +130,16 @@ Marketify.App = ( function($) {
 
       pagi();
 
+      $( '.download-gallery__image' ).magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+          enabled: true,
+          navigateByImgClick: true,
+          preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        }
+      });
+
 		},
 
 		popup : function( args ) {
