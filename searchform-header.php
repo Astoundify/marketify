234c300510c01adeb9a2942086f8b73460744573
Spencer Finnell <spencerfinnell@gmail.com>
2016-01-13 10:48:06 -0500

    Placeholder color. Closes #594

--- searchform-header.php
+++ searchform-header.php
@@ -8,7 +8,7 @@
 
 <form role="search" method="get" class="search-form<?php echo '' != get_search_query() ? ' active' : ''; ?>" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label class="screen-reader-text" for="s"><?php _ex( 'Search for:', 'label', 'marketify' ); ?></label>
-    <input type="search" class="search-field" placeholder="<?php echo esc_attr__( 'Search', 'marketify' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php echo esc_attr__( 'Search for:', 'marketify' ); ?>">
+    <input type="search" class="search-field" placeholder="<?php esc_attr_e( 'Search', 'marketify' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php esc_attr_e( 'Search for:', 'marketify' ); ?>">
     <input type="hidden" name="post_type" value="download" />
 
     <button type="submit" class="search-submit"><span class="screen-reader-text"><?php _e( 'Search', 'marketify' ); ?></span></button>
