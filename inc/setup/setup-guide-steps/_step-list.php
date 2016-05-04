503eec8e31818287f9dea9ffeed7db57a0f96998
Spencer Finnell <spencerfinnell@gmail.com>
2016-05-04 13:08:37 -0400

    Just check for the token to avoid testing request on all page loads.

--- inc/setup/setup-guide-steps/_step-list.php
+++ inc/setup/setup-guide-steps/_step-list.php
@@ -5,24 +5,21 @@
  * @since 2.7.0
  */
 
-/** Some deps */
-$themeforest_api = Astoundify_Envato_Market_API::instance();
-
 /** Create the steps */
 $steps = array();
 
 $steps[ 'theme-updater' ] = array(
 	'title' => __( 'Enable Automatic Updates', 'marketify' ),
-	'completed' => get_option( 'marketify_themeforest_updater_token', null ) && $themeforest_api->can_make_request_with_token() ? true : false
+	'completed' => get_option( 'marketify_themeforest_updater_token', null )
 );
 
 $steps[ 'install-plugins' ] = array(
-	'title' => __( 'Install Required &amp; Recommended Plugins', 'marketify' ),
+	'title' => __( 'Install Plugins', 'marketify' ),
 	'completed' => class_exists( 'Easy_Digital_Downloads' )
 );
 
 $steps[ 'import-content' ] = array(
-	'title' => __( 'Install Demo Content', 'marketify' ),
+	'title' => __( 'Import Content', 'marketify' ),
 	'completed' => 
 		Astoundify_Importer_Manager::has_previously_imported( 'nav_menus' ) &&
 		Astoundify_Importer_Manager::has_previously_imported( 'posts_pages' ) &&
