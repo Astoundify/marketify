ff62800a22b42068da54f3de7ee05027d3071924
Spencer Finnell <spencerfinnell@gmail.com>
2016-05-17 10:38:37 -0400

    Type fix See #654

--- inc/setup/setup-guide-steps/child-theme.php
+++ inc/setup/setup-guide-steps/child-theme.php
@@ -1,4 +1,4 @@
-<p><?php _e( 'Installing a child themem allows for an easy way to add extra customization to your website, as well as ensures any changes made are not ovewritten when your parent theme is updated. <strong>This is highly recommended.</strong>', 'marketify' ); ?></p>
+<p><?php _e( 'Installing a child theme allows for an easy way to add extra customization to your website, as well as ensures any changes made are not ovewritten when your parent theme is updated. <strong>This is highly recommended.</strong>', 'marketify' ); ?></p>
 
 <p><a class="button button-primary button-large uct-activate" href="javascript:;"><?php esc_html_e( 'Use a Child Theme', 'marketify' ); ?></a></p>
 
