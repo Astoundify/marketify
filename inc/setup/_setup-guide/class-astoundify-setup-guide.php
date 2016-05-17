d0faf491d6afdd3ad660ff75b0bc8fa82488db5d
Spencer Finnell <spencerfinnell@gmail.com>
2016-05-17 12:55:56 -0400

    Update setup guide drop-in

--- inc/setup/_setup-guide/class-astoundify-setup-guide.php
+++ inc/setup/_setup-guide/class-astoundify-setup-guide.php
@@ -186,7 +186,7 @@ class Astoundify_Setup_Guide {
 		add_theme_page( 
 			sprintf( self::get_string( 'page-title' ), self::$current_theme->get( 'Name' ) ), 
 			self::get_string( 'menu-title' ),
-			'edit_themes', 
+			'edit_theme_options', 
 			self::$template . '-setup',
 			array( __CLASS__, 'output_admin_page' ) 
 		);
