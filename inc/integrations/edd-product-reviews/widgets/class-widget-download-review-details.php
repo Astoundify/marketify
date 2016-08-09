e9801c0f8e7894095d29036a580c284d53084ba1
Spencer Finnell <spencerfinnell@gmail.com>
2016-08-09 15:17:34 -0400

    Ensure function exists for widget title.g

--- inc/integrations/edd-product-reviews/widgets/class-widget-download-review-details.php
+++ inc/integrations/edd-product-reviews/widgets/class-widget-download-review-details.php
@@ -6,7 +6,7 @@ class Marketify_Widget_Download_Review_Details extends Marketify_Widget {
         $this->widget_cssclass    = 'marketify_widget_download_review_details';
         $this->widget_description = __( 'Display average review information.', 'marketify' );
         $this->widget_id          = 'marketify_widget_download_review_details';
-        $this->widget_name        = sprintf( __( 'Marketify - %1$s: Review Details', 'marketify' ), edd_get_label_singular() );
+        $this->widget_name        = sprintf( __( 'Marketify - %1$s: Review Details', 'marketify' ), function_exists( 'edd_get_label_singular' ) ? edd_get_label_singular() : 'Download' );
         $this->settings           = array(
             'title' => array(
                 'type'  => 'text',
