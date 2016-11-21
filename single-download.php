2f58f2c0fe5126dbddb2826ef5616a64bdfcf400
Spencer Finnell <spencerfinnell@gmail.com>
2016-11-21 10:01:19 -0500

    Move loop around comments so microdata can catch comments as children.

--- single-download.php
+++ single-download.php
@@ -11,15 +11,14 @@ get_header(); ?>
 
     <?php do_action( 'marketify_entry_before' ); ?>
 
+	<?php while ( have_posts() ) : the_post(); ?>
+
     <div class="container">
         <div id="content" class="site-content row">
 
             <div role="main" class="content-area <?php echo ! is_active_sidebar( 'sidebar-download-single' ) ? 'col-xs-12' : 'col-xs-12 col-md-8'; ?>">
 
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <?php get_template_part( 'content-single', 'download' ); ?>
-                <?php endwhile; rewind_posts(); ?>
-
+				<?php get_template_part( 'content-single', 'download' ); ?>
             </div>
 
             <?php get_sidebar( 'single-download' ); ?>
@@ -30,5 +29,7 @@ get_header(); ?>
 
         <?php do_action( 'marketify_single_download_after' ); ?>
     </div>
+
+    <?php endwhile; ?>
 
 <?php get_footer(); ?>
