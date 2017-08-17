0f431d7e98037166a4e6ef4b75bf7341d0c0e4dd
Spencer Finnell <spencerfinnell@gmail.com>
2017-08-17 11:54:24 -0400

    Import new widgets. See #713

--- footer.php
+++ footer.php
@@ -15,47 +15,14 @@
 			<div class="footer-widget-areas">
 				<div class="widget widget--site-footer">
 					<?php dynamic_sidebar( 'footer-1' ); ?>
-
-					<?php
-					if ( ! is_active_widget( false, false, 'marketify-widget-footer-social-icons', true ) ) {
-						the_widget( 'Marketify_Widget_Footer_Social_Icons', array(), array(
-							'before_widget' => '<aside>',
-							'after_widget'  => '</aside>',
-							'before_title'  => '<h3 class="widget-title widget-title--site-footer">',
-							'after_title'   => '</h3>',
-						) );
-					}
-					?>
 				</div>
 
 				<div class="widget widget--site-footer">
 					<?php dynamic_sidebar( 'footer-2' ); ?>
-
-					<?php
-					if ( ! is_active_widget( false, false, 'marketify-widget-footer-contact', true ) ) {
-						the_widget( 'Marketify_Widget_Footer_Contact', array(), array(
-							'before_widget' => '<aside>',
-							'after_widget'  => '</aside>',
-							'before_title'  => '<h3 class="widget-title widget-title--site-footer">',
-							'after_title'   => '</h3>',
-						) );
-					}
-					?>
 				</div>
 
 				<div class="widget widget--site-footer">
 					<?php dynamic_sidebar( 'footer-3' ); ?>
-
-					<?php
-					if ( ! is_active_widget( false, false, 'marketify-widget-footer-copyright', true ) ) {
-						the_widget( 'Marketify_Widget_Footer_Copyright', array(), array(
-							'before_widget' => '<aside>',
-							'after_widget'  => '</aside>',
-							'before_title'  => '<h3 class="widget-title widget-title--site-footer">',
-							'after_title'   => '</h3>',
-						) );
-					}
-					?>
 				</div>
 			</div>
 
