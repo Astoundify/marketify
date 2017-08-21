71c41fe574c913165be58f734d05ab848e7ca2e5
Spencer Finnell <spencerfinnell@gmail.com>
2017-08-21 14:22:20 -0400

    Fix theme mod setting.

--- inc/customizer/definitions/controls/download-archives-per-page.php
+++ inc/customizer/definitions/controls/download-archives-per-page.php
@@ -9,12 +9,12 @@ if ( ! defined( 'ABSPATH' ) || ! $wp_customize instanceof WP_Customize_Manager )
 	exit; // Exit if accessed directly.
 }
 
-$wp_customize->add_setting( 'downloads-archives-per-pages', array(
+$wp_customize->add_setting( 'downloads-archives-per-page', array(
 	'default' => 12,
 	'sanitize_callback' => 'absint',
 ) );
 
-$wp_customize->add_control( 'downloads-archives-per-pages', array(
+$wp_customize->add_control( 'downloads-archives-per-page', array(
 	'label' => sprintf( __( '%s Per Page', 'marketify' ), edd_get_label_plural() ),
 	'type' => 'number',
 	'description' => __( 'Can be overwritten by passing <code>number</code> to the <code>[downloads]</code> shortcode', 'marketify' ),
